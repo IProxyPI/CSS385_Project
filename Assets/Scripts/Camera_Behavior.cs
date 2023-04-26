eff2ef8dcc5dd65071632e94222ac4f4f1588a62
MinNguTru <85091785+MinNguTru@users.noreply.github.com>
2023-04-26 16:28:51 -0700

    Basic Movement and Camera prototype commit

---
+++ Assets/Scripts/Camera_Behavior.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Behavior : MonoBehaviour
+{
+    public GameObject Player1, Player2;
+    public GameObject ChaseCamera;
+    private Vector3 position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+        position.z = -10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocity = Vector3.zero;
+        // if (Mathf.Abs(Player1.transform.position.x - Player2.transform.position.x) > 7)
+        // {
+        position.x = (Player1.transform.position.x + Player2.transform.position.x) / 2;
+
+        // }
+        transform.position = Vector3.SmoothDamp(transform.position, position, ref velocity, .4f);
+    }
+}
