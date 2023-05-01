ddbb9b4f17dc6afcd56838100358cb51be3f7520
Proxy <pitzelgabriel@gmail.com>
2023-05-01 14:12:57 -0700

    Further tweaked the camera.

--- Assets/Scripts/Camera_Behavior.cs
+++ Assets/Scripts/Camera_Behavior.cs
@@ -8,7 +8,7 @@ public class Camera_Behavior : MonoBehaviour
     public GameObject ChaseCamera;
     private Vector3 position;
 
-    public float speed = 1.5f;
+    private float speed = 3.5f;
     // Start is called before the first frame update
     void Start()
     {
