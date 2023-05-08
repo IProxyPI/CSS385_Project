6907656b21bc597c4a9c0760ab406802de82da2b
Proxy <pitzelgabriel@gmail.com>
2023-05-08 16:22:45 -0700

    Once round ends, should pause longer now before moving to next- Game *should* freeze during this period

--- Assets/Scripts/Round_Vis_Manager.cs
+++ Assets/Scripts/Round_Vis_Manager.cs
@@ -14,7 +14,7 @@ public static class Game_Data
     
     public static float current_timescale = 1f;
     public static bool freeze_frame = false; // Doesnt do anything yet, but ideally we should slap this little guy into every
-    // update function
+    
 }
 
 public class Round_Vis_Manager : MonoBehaviour
@@ -23,6 +23,8 @@ public class Round_Vis_Manager : MonoBehaviour
     private int victory_condition = 3; // Round wins required to win the game
 
     private float freezer = 0;
+    private float end_of_round_countdown = 2f;
+    private bool round_over = false;
 
     private int p1hp = 2;
     private int p2hp = 2;
@@ -80,15 +82,9 @@ public class Round_Vis_Manager : MonoBehaviour
         {
             Game_Data.p1_wins++;
         }
-        
-        if (Game_Data.p1_wins < victory_condition && Game_Data.p2_wins < victory_condition)
-        {
-            SceneManager.LoadScene("1_Fight");
-        }
-        else
-        {
-            SceneManager.LoadScene("2_Victory");
-        }
+
+        round_over = true;
+        Apply_Freezeframe(end_of_round_countdown);
     }
         
 
@@ -124,6 +120,22 @@ public class Round_Vis_Manager : MonoBehaviour
             Game_Data.freeze_frame = true;
             freezer -= Time.fixedDeltaTime;
             Time.timeScale = 0;
+        }
+
+        if (round_over)
+        {
+            end_of_round_countdown -= Time.deltaTime;
+            if (end_of_round_countdown <= 0)
+            {
+                if (Game_Data.p1_wins < victory_condition && Game_Data.p2_wins < victory_condition)
+                {
+                    SceneManager.LoadScene("1_Fight");
+                }
+                else
+                {
+                    SceneManager.LoadScene("2_Victory");
+                }
+            }
         }
     }
     
