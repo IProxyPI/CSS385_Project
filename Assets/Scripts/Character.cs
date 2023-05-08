6866e60209bbc8816b3c48d0da0e2d995c901788
Jaimi Chong <jaimi.chong@gmail.com>
2023-05-07 23:59:12 -0700

    Reverted switch case, Fixed backdashes w/ temp sol

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -8,28 +8,29 @@ public class Character : MonoBehaviour
     private string p1_name = "DummyPlayer1";
     private string p2_name = "DummyPlayer2";
 
-    public string case_movement;
+    // Movement
     public float forward = 0;
     public float forward_stop = 0;
     public float backward = 0;
     public float backward_stop = 0;
+    private float step = 0.1f;
 
-    public string case_action;
+    // Actions
     public float attack = 0;
     public float attack_end = 1;
     public float stun = 0;
     public float stun_end = 1;
     public float block = 0;
     public float block_stop = 0;
-
-    private float all_stop_ends = 0.5f;
-    private float step = 0.1f;
     private GameObject attack_prefab;
     private GameObject stun_prefab;
     private GameObject block_prefab;
     private bool attack_instantiated = false;
     private bool stun_instantiated = false;
     private bool block_instantiated = false;
+
+    // Both
+    private float all_stop_ends = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -47,81 +48,94 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        switch (case_movement)
+        // Movement
         {
-            case "forward":
+            // forward
+            if (forward > 0)
+            {
                 pc.rb.position = new Vector3(pc.transform.position.x + step, pc.transform.position.y, 0);
-                forward = Endless_Animation_Counter(forward);
-                break;
-
-            case "forward_stop":
-                forward_stop = Endless_Animation_Stopper(forward_stop);
-                break;
+                Endless_Animation_Counter(ref forward);
+            }
             
-            case "backward":
+            // backward
+            if (backward > 0)
+            {
                 pc.rb.position = new Vector3(pc.transform.position.x - step, pc.transform.position.y, 0);
-                backward = Endless_Animation_Counter(backward);
-                break;
-            
-            case "backward_stop":
-               backward_stop = Endless_Animation_Stopper(backward_stop);
-               break;
+                Endless_Animation_Counter(ref backward);
+            }
+
+            // forward_stop
+            if (forward_stop > 0)
+            {
+                Endless_Animation_Stopper(ref forward, ref forward_stop);
+            }
+
+            // backward_stop
+            if (backward_stop > 0)
+            {
+                Endless_Animation_Stopper(ref backward, ref backward_stop);
+            }
         }
 
-        switch (case_action)
+        // Actions
         {
-            case "attack":
+            // attack
+            if (attack > 0)
+            {
                 if (!attack_instantiated)
                 {
                     // instantiate attack_prefab
                     attack_instantiated = true;
                 }
                 Set_Animation_Counter(attack, attack_end, attack_prefab, attack_instantiated);
-                break;
+            }
 
-            case "stun":
+            // stun
+            if (stun > 0)
+            {
                 if (!stun_instantiated)
                 {
                     // instantiate stun_prefab
                     stun_instantiated = true;
                 }
                 Set_Animation_Counter(stun, stun_end, stun_prefab, stun_instantiated);          
-                break;
+            }
 
-            case "block":
+            // block
+            if (block > 0)
+            {
                 if (!block_instantiated)
                 {
                     // instantiate block_prefab
                     block_instantiated = true;
                 }
-                Endless_Animation_Counter(block);
-                break;
+                Endless_Animation_Counter(ref block);
+                }
 
-            case "block_stop":
-                block_stop = Endless_Animation_Stopper(block_stop);
-                break;
+            // block_stop
+            if (block_stop > 0) {
+                Endless_Animation_Stopper(ref block, ref block_stop);
+            }
         }
     }
 
-    private float Endless_Animation_Counter(float action)
+    private void Endless_Animation_Counter(ref float action)
     {
-        return action += Time.deltaTime;
+        action += Time.fixedDeltaTime;
     }
-    
-    private float Endless_Animation_Stopper(float action_stop)
+
+    private void Endless_Animation_Stopper(ref float action, ref float action_stop)
     {
-        // if (block_prefab != null)
-        // {
-        //     // destroy block_prefab
-        //     block_instantiated = false;
-        // }
         if (action_stop >= all_stop_ends)
         {
-            case_movement = null;
+            action_stop = 0;
             pc.actionable = true;
-            return action_stop = 0;
         }
-        return action_stop += Time.deltaTime;
+        else
+        {
+            action = 0;
+            action_stop += Time.fixedDeltaTime;
+        }
     }
 
     private void Set_Animation_Counter (float action, float action_end, GameObject action_prefab, bool action_instantiated)
@@ -129,7 +143,7 @@ public class Character : MonoBehaviour
         if (action > 0)
         {
             Debug.Log("P" + pc.player + " has been definitely acting for " + action + " seconds");
-            action += Time.deltaTime;
+            action += Time.fixedDeltaTime;
             if (action >= action_end)
             {
                 if (action_prefab != null)
